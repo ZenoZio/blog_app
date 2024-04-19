91cf62f7958ec51883519df59a2944d879b4b01a
abcZenoZio <ngoctrieulk1118@gmail.com>
2024-04-19 10:09:54 +0700

    connect to the database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 const router = express.Router();
+
+// QjHIP95MIKb48wog
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
