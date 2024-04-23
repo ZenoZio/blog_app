f2bf54fc37a8f1c1fa6701ef979083bce537bbf3
abcZenoZio <ngoctrieulk1118@gmail.com>
2024-04-23 14:32:57 +0700

    conplete user image upload function

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -14,7 +14,7 @@ const DashSidebar = () => {
     }
   }, [location.search]);
   return (
-    <Sidebar className="w-full md:w-56 ">
+    <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
           <Link to="/dashboard?tab=profile">
