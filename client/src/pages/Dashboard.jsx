532980ada2916373ff585c3eb7997bf23ea72cfb
abcZenoZio <ngoctrieulk1118@gmail.com>
2024-05-13 11:51:12 +0700

    add dashboard overviwe to the admin

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
 import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashboardComp";
 const Dashboard = () => {
   const location = useLocation();
   const [tab, setTab] = useState("");
@@ -28,8 +29,9 @@ const Dashboard = () => {
       {/* Users */}
       {tab === "users" && <DashUsers />}
       {/* comments */}
-      {tab === 'comments' && <DashComments />  }
-
+      {tab === "comments" && <DashComments />}
+      {/* dashboard comp */}
+      {tab === "dash" && <DashboardComp />}
     </div>
   );
 };
